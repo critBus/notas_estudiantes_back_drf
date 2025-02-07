11096d43b4b6b1a2f646d30ea87d11661b244ca3
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-02-07 16:08:39 -0500

    permitir agregar grupos

--- apps/users/admin/group/admin.py
+++ apps/users/admin/group/admin.py
@@ -28,11 +28,11 @@ class CustomRoleAdmin(admin.ModelAdmin):
         "name",
     )
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
+    #
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+    #
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
