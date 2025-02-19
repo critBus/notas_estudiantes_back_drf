88aba75a3124a52bfc8674414e89a0c6d41418d0
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-02-19 14:02:38 -0500

    test grant view

--- tests/student/test_grant_career.py
+++ tests/student/test_grant_career.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse
 
 from apps.project.models import (
     Career,
@@ -68,3 +69,72 @@ class TestGrantCareer(DegreeEscaleTestCase):
             self.assertTrue(
                 q.filter(student=student, career=careers[i]).exists()
             )
+
+    def call_grant_career(
+        self,
+        unauthorized: bool = False,
+        forbidden: bool = False,
+        bad_request: bool = False,
+        not_found: bool = False,
+        print_json_response: bool = False,
+    ):
+        URL = reverse("grant-career-grant")
+        response_dict = self.call_get(
+            url=URL,
+            unauthorized=unauthorized,
+            forbidden=forbidden,
+            bad_request=bad_request,
+            not_found=not_found,
+            print_json_response=print_json_response,
+        )
+
+        return response_dict
+
+    def test_grant_career_view(self):
+        students, careers = self.create_ballots_to_students()
+        DegreeScale.calculate_all_ranking_number()
+
+        response_dict = self.call_grant_career(print_json_response=True)
+
+        q = GrantCareer.objects.all()
+        self.assertEqual(q.count(), 3)
+        for i, student in enumerate(students):
+            self.assertTrue(
+                q.filter(student=student, career=careers[i]).exists()
+            )
+
+        self.assertEqual(
+            response_dict,
+            [
+                {
+                    "id": grant.id,
+                    "student": {
+                        "id": grant.student.id,
+                        "is_approved": True,
+                        "ci": grant.student.ci,
+                        "address": grant.student.address,
+                        "grade": grant.student.grade,
+                        "last_name": grant.student.last_name,
+                        "first_name": grant.student.first_name,
+                        "registration_number": grant.student.registration_number,
+                        "sex": grant.student.sex,
+                        "is_graduated": True,
+                        "is_dropped_out": False,
+                    },
+                    "school_year": {
+                        "id": grant.school_year.id,
+                        "start_date": str(grant.school_year.start_date),
+                        "end_date": str(grant.school_year.end_date),
+                        "name": grant.school_year.name,
+                    },
+                    "career": {
+                        "id": grant.career.id,
+                        "amount": grant.career.amount,
+                        "name": grant.career.name,
+                    },
+                }
+                for grant in [
+                    GrantCareer.objects.get(student=v) for v in students
+                ]
+            ],
+        )
