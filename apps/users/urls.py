db25a51432b25ec65805810f2fc0538f53998801
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-03-02 17:48:17 -0500

    groups?

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path("token/", include("apps.users.views.auth.urls")),
     path("users/", include("apps.users.views.user.urls")),
+    path("groups/", include("apps.users.views.group.urls")),
 ]
