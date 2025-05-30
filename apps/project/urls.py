6d00ac96cb48aa25ca79771770df16b9dd654caa
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-05-30 19:48:27 -0400

    add StudentNoteMultipleByStudentView

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -51,6 +51,7 @@ from apps.project.views import (
     Upgrading7and8,
     UpgradingAllView,
     WithoutGrantingView,
+    StudentNoteMultipleByStudentView
 )
 
 router = DefaultRouter()
@@ -113,6 +114,11 @@ urlpatterns = [
         "student_note/multiple/",
         StudentNoteMultipleCreateView.as_view(),
         name="student-note-create-multiple",
+    ),
+    path(
+        "student_note/multiple/bySudent/<int:pk>/",
+        StudentNoteMultipleByStudentView.as_view(),
+        name="student-note-multiple",
     ),
     path(
         "student_note/multiple/<int:pk>/",
